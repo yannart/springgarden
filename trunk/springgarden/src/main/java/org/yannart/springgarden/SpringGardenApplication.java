cb86d2fac299a8ce6a1a2fa50b6c64a5a833198d
yannart <yannart@7ebc68e6-cb97-11de-a8df-5d31f88aefc0>
2009-11-07 15:09:46 +0000

    Déplacement du fichier de configuration de Spring.

--- trunk/springgarden/src/main/java/org/yannart/springgarden/SpringGardenApplication.java
+++ trunk/springgarden/src/main/java/org/yannart/springgarden/SpringGardenApplication.java
@@ -14,26 +14,26 @@ public class SpringGardenApplication {
 	 */
 	public static void main(String[] args) {
 
-		// On lit le fichier de configuration de Spring
+		// Lit le fichier de configuration de Spring
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml");
 
-		// On recupere l'objet Jardin a partir du Bean Spring */
+		// Recupere l'objet Jardin a partir du Bean Spring */
 		IJardin jardin = context.getBean("jardin", IJardin.class);
  
-		// On montre le contenu du jardin */
+		// Montre le contenu du jardin */
 		System.out.println("\nJARDIN DE DEPART");
 		jardin.listerParcelles();
  
-		// On arrose tout le jardin */
+		// Arrose tout le jardin */
 		System.out.println("\nON ARROSE LE JARDIN");
 		jardin.arrose();
 
-		// On met de l'engrais a tout le jardin */
+		// Met de l'engrais a tout le jardin */
 		System.out.println("\nON MET DE L'ENGRAIS DANS LE JARDIN");
 		jardin.mettreEngrais();
 
-		// On montre le contenu du jardin */
+		// Montre le contenu du jardin */
 		System.out.println("\nJARDIN A LA FIN");
 		jardin.listerParcelles();
 	}
